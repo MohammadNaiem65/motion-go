f1de882aa8ca64b1ec7018deb8ea18126b36c8a9
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-01-07 19:48:00 +0600

    add: started the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+    return (
+        <div className='h-[350dvh] bg-[radial-gradient(ellipse_at_bottom,_#1b2735_0%,#090a0f_100%)]'>
+            <div className='w-full h-dvh pt-[150dvh]  flex justify-center items-center'>
+                <h2 className="text-2xl text-white">Motion Go</h2>
+            </div>
+        </div>
+    );
+}
+
+export default App;
